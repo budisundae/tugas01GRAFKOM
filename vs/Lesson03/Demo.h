69626f48059a4388c7b03c166d4cac9286bf4216
budisundae <budiswant46@gmail.com>
2021-09-26 11:06:29 +0700

    first init

---
+++ vs/Lesson03/Demo.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "RenderEngine.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/vector_angle.hpp>
+#include <SOIL/SOIL.h>
+
+class Demo :
+	public RenderEngine
+{
+public:
+	Demo();
+	~Demo();
+private:
+	GLuint shaderProgram, VBO, VAO, EBO, texture, VBO2, VAO2, EBO2, texture2, VBO3, VAO3, EBO3, texture3, VBO4, VAO4, EBO4, texture4, VBO5, VAO5, EBO5, texture5;
+	virtual void Init();
+	virtual void DeInit();
+	virtual void Update(double deltaTime);
+	virtual void Render();
+	virtual void ProcessInput(GLFWwindow *window);
+	void BuildKulkasDepan();
+	void BuildKulkasSamping();
+	void BuildKulkasAtas();
+	void BuildColoredPlane();
+	void DrawKulkasDepan();
+	void DrawKulkasSamping();
+	void DrawKulkasAtas();
+	void DrawColoredPlane();
+};
+
